e59b523ee4aeca8970b2fe74a212f1ae7cd57c5b
martin <jdimartino@simtlix.com>
2021-04-27 10:12:44 -0300

    se agregaron metodos al utils padre dentro de la carpeta components

--- server/components/utils/Utils.ts
+++ server/components/utils/Utils.ts
@@ -1,8 +1,36 @@
-
+import Constants from '../Constants';
 abstract class Utils {
 
     public exists: Function = (anObject: any): boolean => {
         return anObject && Object.keys(anObject).length > 0;
+    }
+
+    public getCondition: Function = (param: string): any => {
+        let splitParam: string[] = param.split(':');
+        if (splitParam.length === 1) {
+            // By default it will be compared by equals
+            return this.buildCondition(Constants.EQ_OPERATOR, splitParam[0]);
+        }
+        return this.buildCondition(splitParam[0], splitParam[1]);
+    }
+
+    // Privates Functions
+    private buildCondition: Function = (criteria: string, values: string): any => {
+        let aCondition: any = {};
+        aCondition.criteria = criteria;
+        aCondition.values = this.splitValues(values);
+
+        return aCondition;
+    }
+
+    private splitValues: Function = (values: string) => {
+        let splitParams: any[] = values.split(',');
+        for (let i = 0; i < splitParams.length; i++) {
+            if (!isNaN(splitParams[i])) {
+                splitParams[i] = parseInt(splitParams[i], 10);
+            }
+        }
+        return splitParams;
     }
 }
 
